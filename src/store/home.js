3a72f43366dcb91f4a52ea533615ad0877765e4e
Ismail <ismailsmakhus@gmail.com>
2020-11-02 22:37:41 +0700

    make promise fetchPokemons

--- src/store/home.js
+++ src/store/home.js
@@ -11,29 +11,33 @@ export default {
     }
   },
   actions: {
-    async fetchPokemons(context) {
-      if (context.state.pokemons.length == 0) {
-        try {
-          let response = await fetch(`${baseApi}?limit=100&offset=0`)
-          let resjson = await response.json()
-          let results = resjson['results']
-          let pokemons = []
-          results.forEach(pokemon => {
-            // remove last slash from pokemon url
-            let pokemonUrl = pokemon['url'].slice(0, -1)
-            // get pokemon id
-            let pokemonId = pokemonUrl.split('/').pop()
-  
-            pokemons.push({
-              'id': pokemonId,
-              'name': pokemon['name']
+    fetchPokemons(context) {
+      return new Promise(async (resolve, reject) => {
+        if (context.state.pokemons.length == 0) {
+          try {
+            let response = await fetch(`${baseApi}?limit=100&offset=0`)
+            let resjson = await response.json()
+            let results = resjson['results']
+            let pokemons = []
+            results.forEach(pokemon => {
+              // remove last slash from pokemon url
+              let pokemonUrl = pokemon['url'].slice(0, -1)
+              // get pokemon id
+              let pokemonId = pokemonUrl.split('/').pop()
+    
+              pokemons.push({
+                'id': pokemonId,
+                'name': pokemon['name']
+              })
             })
-          })
-          context.commit('updatePokemons', pokemons)
-        } catch (err) {
-          console.log(err)
+            context.commit('updatePokemons', pokemons)
+          } catch (err) {
+            reject('Something error.')
+          }
         }
-      }
+  
+        resolve(null)
+      })
     }
   }
 }
